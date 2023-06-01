26f7b19c7fcae0580d2b880a01ceacbc8a1f03be
Ben Cheung <87406737+BCCheungGit@users.noreply.github.com>
2023-06-01 16:17:23 -0400

    Project created

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 
+            '08221f0e07msh9ce2a8ae74c15fbp16a631jsn675a3a8e5553')
+            
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' }),
+    }),
+});
+
+
+export const {
+    useGetTopChartsQuery,
+} = shazamCoreApi;
