97843902941632cd3a2c65f25cf37993e75f7957
Ben Cheung <87406737+BCCheungGit@users.noreply.github.com>
2023-06-01 20:26:35 -0400

    started api call

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -4,12 +4,13 @@ import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-
+import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
 
 const SongDetails = () => {
     const dispatch = useDispatch();
     const { songid } = useParams();
     const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data: songData, isFetching: isFetchingSongDetails} = useGetSongDetailsQuery({ songid });
     console.log(songid);
 
     return (
@@ -19,6 +20,13 @@ const SongDetails = () => {
                 songData={songData}
             />
             */}
+            <div className="mb-10">
+                <h2 className="text-white text-3xl font-bold">Lyrics: 
+                </h2>
+
+                <div className="mt-5">
+                </div>
+            </div>
         </div>
     )
 
